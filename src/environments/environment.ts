ae31e6e9b9de2f0142ee567900622ea566626bc7
Joel Aufgang <jaufgang@cisco.com>
2022-04-10 16:39:54 -0400

    added Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'apsc-46801',
+    appId: '1:105485068586:web:b9dea83c9e52e6a722d653',
+    storageBucket: 'apsc-46801.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCQoJuJkaMJ1TAKZw03Y0bQvrSHtQILbqM',
+    authDomain: 'apsc-46801.firebaseapp.com',
+    messagingSenderId: '105485068586',
+  },
   production: false
 };
 
